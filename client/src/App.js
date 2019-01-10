d80bdc6d75df8083165259e684ba3bcb19b25deb
Kent <kentsaeteurn@gmail.com>
2019-01-10 13:21:39 -0800

    unnecessary code and packages; change commands for heroku-postbuild

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,6 @@ import './App.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {}
-
-    this.connectToServer = this.connectToServer.bind(this);
-  }
-
-  connectToServer() {
-    fetch('/');
-  }
-
-  componentDidMount() {
-    this.connectToServer();
-  }
-
   render() {
     return (
       <div className="App">
